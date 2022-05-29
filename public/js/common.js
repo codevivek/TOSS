834e7a6f3e69c0ee37c257eb2e00dc67c5f0f3fb
Vivek Bisht <vivekbisht107@gmail.com>
2022-05-28 21:41:08 -0400

    View Posts

--- public/js/common.js
+++ public/js/common.js
@@ -53,11 +53,35 @@ $("#replyModal").on("show.bs.modal", (event) => {
     $("#submitReplyButton").data("id", postId);
 
     $.get("/api/posts/" + postId, results => {
-        outputPosts(results, $("#originalPostContainer"));
+        outputPosts(results.postData, $("#originalPostContainer"));
     })
 })
 
 $("#replyModal").on("hidden.bs.modal", () => $("#originalPostContainer").html(""));
+
+$("#deletePostModal").on("show.bs.modal", (event) => {
+    var button = $(event.relatedTarget);
+    var postId = getPostIdFromElement(button);
+    $("#deletePostButton").data("id", postId);
+})
+
+$("#deletePostButton").click((event) => {
+    var postId = $(event.target).data("id");
+
+    $.ajax({
+        url: `/api/posts/${postId}`,
+        type: "DELETE",
+        success: (data, status, xhr) => {
+
+            if(xhr.status != 202) {
+                alert("could not delete post");
+                return;
+            }
+            
+            location.reload();
+        }
+    })
+})
 
 $(document).on("click", ".likeButton", (event) => {
     var button = $(event.target);
@@ -108,6 +132,15 @@ $(document).on("click", ".retossButton", (event) => {
 
 })
 
+$(document).on("click", ".post", (event) => {
+    var element = $(event.target);
+    var postId = getPostIdFromElement(element);
+
+    if(postId !== undefined && !element.is("button")) {
+        window.location.href = '/posts/' + postId;
+    }
+});
+
 function getPostIdFromElement(element) {
     var isRoot = element.hasClass("post");
     var rootElement = isRoot == true ? element : element.closest(".post");
@@ -118,7 +151,7 @@ function getPostIdFromElement(element) {
     return postId;
 }
 
-function createPostHtml(postData) {
+function createPostHtml(postData, largeFont=false) {
     
     if(postData == null) return alert("post object is null");
 
@@ -136,6 +169,7 @@ function createPostHtml(postData) {
     var timestamp = timeDifference(new Date(), new Date(postData.createdAt));
     var likeButtonActiveClass = postData.likes.includes(userLoggedIn._id) ? "active" : "";
     var retossButtonActiveClass = postData.retossUsers.includes(userLoggedIn._id) ? "active" : "";
+    var largeFontClass = largeFont ? "largeFont" : "";
 
     var retossText = '';
     if(isRetoss) {
@@ -145,8 +179,8 @@ function createPostHtml(postData) {
                     </span>`
     }
 
-    var replyFlag = "";
-    if(postData.replyTo) {
+    var replyFlag = '';
+    if(postData.replyTo && postData.replyTo._id) {
         
         if(!postData.replyTo._id) {
             return alert("Reply to is not populated");
@@ -158,11 +192,16 @@ function createPostHtml(postData) {
         var replyToUsername = postData.replyTo.postedBy.userName;
         replyFlag = `<div class='replyFlag'>
                         Replying to <a href='/profile/${replyToUsername}'>@${replyToUsername}<a>
-                    </div>`;
+                    </div>`
 
     }
 
-    return `<div class='post' data-id='${postData._id}'>
+    var buttons = "";
+    if (postData.postedBy._id == userLoggedIn._id) {
+        buttons = `<button data-id="${postData._id}" data-toggle="modal" data-target="#deletePostModal"><i class='bx bx-x'></i></button>`;
+    }
+
+    return `<div class='post ${largeFontClass}' data-id='${postData._id}'>
                 <div class='postActionContainer'>
                     ${retossText}
                 </div>
@@ -175,6 +214,7 @@ function createPostHtml(postData) {
                             <a href='/profile/${postedBy.userName}' class='displayName'>${displayName}</a>
                             <span class='username'>@${postedBy.userName}</span>
                             <span class='date'>${timestamp}</span>
+                            ${buttons}
                         </div>
                         <div class='postBody'>
                             <span>${postData.content}</span>
@@ -256,4 +296,21 @@ function outputPosts(results, container) {
     if (results.length == 0) {
         container.append("<span class='noResults'>Nothing to show.</span>")
     }
+}
+
+function outputPostsWithReplies(results, container) {
+    container.html("");
+
+    if(results.replyTo !== undefined && results.replyTo._id !== undefined) {
+        var html = createPostHtml(results.replyTo)
+        container.append(html);
+    }
+
+    var mainPostHtml = createPostHtml(results.postData, true)
+    container.append(mainPostHtml);
+
+    results.replies.forEach(result => {
+        var html = createPostHtml(result)
+        container.append(html);
+    });
 }
