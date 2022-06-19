60720900c577c3c9fb1c4dc88177c65e64476217
JennyAdroja <jennycollege2@gmail.com>
2022-06-18 20:24:55 -0400

    port changes

--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -1,20 +1,21 @@
 var connected = false;
-
-var socket = io("http://localhost:4000")
+ 
+let socket = io(' https://tossnetwork.herokuapp.com/ ') 
+// var socket = io("http://localhost:4000")
 socket.emit("setup", userLoggedIn);
-
+ 
 socket.on("connected", () => connected = true);
 socket.on("message received", (newMessage) => messageReceived(newMessage));
-
+ 
 socket.on("notification received", () => {
     $.get("/api/notifications/latest", (notificationData) => {
         showNotificationPopup(notificationData)
         refreshNotificationsBadge();
     })
 })
-
+ 
 function emitNotification(userId) {
     if(userId == userLoggedIn._id) return;
-
+ 
     socket.emit("notification received", userId);
 }
